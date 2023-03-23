53925e7300bb5ec36bd5c3d6bbeca985e885871e
MattHendo <121328628+MattHendo@users.noreply.github.com>
2023-03-22 20:27:38 -0400

    lab 7

---
+++ lab_assignment_7.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int array[] = {97, 16, 45, 63, 13, 22, 7, 58, 72};
+    int n = sizeof(array) / sizeof(int);
+    int i, j, temp, swpcount;
+    for (i = 0; i < n - 1; i++) {
+        swpcount = 0;
+        for (j = 0; j < n - i - 1; j++) {
+            if (array[j] > array[j+1]) {
+                temp = array[j];
+                array[j] = array[j+1];
+                array[j+1] = temp;
+                swpcount++; 
+                /*if I understand the assignment correctly, this should
+                just be a bubble sort with a counter after every swap*/
+            }
+        }
+        printf("pass #%d: %d\n", i+1, swpcount);
+    }
+    return 0;
+}
